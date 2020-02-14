0acdf9e59be22537b626492c1006dd3f8f572a60
Hiroyuki Koshizawa <hiro@HirokMacPro.local>
2020-02-14 21:43:06 +0900

    implement square

--- Sources/Square/Square.swift
+++ Sources/Square/Square.swift
@@ -1,3 +1,7 @@
 public func square(of value: Int) -> Int {
     return value * value
 }
+
+public func square(of value: Double) -> Double {
+    return value * value
+}
--- Tests/SquareTests/SquareTests.swift
+++ Tests/SquareTests/SquareTests.swift
@@ -4,5 +4,6 @@ import XCTest
 final class SquareTests: XCTestCase {
     func testSquare() {
         XCTAssertEqual(square(of: 10), 100)
+        XCTAssertEqual(square(of: 10.0), 100.0)
     }
 }
